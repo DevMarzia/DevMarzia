f5e2afba8ebb2d8cd5dab7401257b581f6378656
Marzia Bonsegna <126612092+DevMarzia@users.noreply.github.com>
2026-02-07 20:37:49 +0100

    Update generate_quote.py

--- generate_quote.py
+++ generate_quote.py
@@ -2,7 +2,7 @@ import random
 import json
 import textwrap
 
-# 1. Carica le frasi
+# 1. Caricamento delle frasi
 with open('quotes.json', 'r') as f:
     quotes = json.load(f)
 
@@ -15,7 +15,7 @@ text_tags = ""
 y_start = 80 - (len(lines) * 5)
 
 for i, line in enumerate(lines):
-    # Aggiungiamo le virgolette solo dove servono
+    # Aggiunge le virgolette solo dove servono
     display_line = line
     if i == 0:
         display_line = f'"{display_line}' # Solo all'inizio della prima riga
@@ -27,7 +27,7 @@ for i, line in enumerate(lines):
 
 y_author = y_start + (len(lines) * 25) + 10
 
-# 3. Costruiamo l'SVG finale
+# 3. Costruzione dell'SVG finale
 svg_template = f'''<svg width="500" height="200" viewBox="0 0 500 200" xmlns="http://www.w3.org/2000/svg">
   <rect width="490" height="190" x="5" y="5" rx="15" fill="#0d1117" stroke="#f06292" stroke-width="3"/>
   {text_tags}
